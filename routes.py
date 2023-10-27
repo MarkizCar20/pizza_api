dcc758b071ab2993c786d0189d787589826b9739
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2023-10-27 19:57:04 +0200

    Fix bugs in routes

--- routes.py
+++ routes.py
@@ -11,7 +11,18 @@ menu = [
     #Add more items - Or add a method to add more items I guesss?
 ]
 
+users = []
+
 orders = []
+
+def check_user_exists(target_user):
+    return any(
+        user.username == target_user.username and
+        user.email == target_user.email and
+        user.address == target_user.address
+        for user in users
+    )
+
 
 @app.route("/admin/menu", methods=["POST"])
 def add_menu_item():
@@ -42,6 +53,12 @@ def delete_item(item_name):
         return jsonify({"message": f"Menu item {item_name} removed from menu"}), 200
     return jsonify({"message": f"Menu item {item_name} not found!"}), 404
 
+@app.route("/admin/order/cancel/<int:order_id>", methods=["DELETE"])
+def cancel_order_admin(order_id):
+    if 0 <= order_id < len(orders):
+        orders.pop(order_id)
+        return jsonify({"message":"Order canceled"})
+
 @app.route("/menu", methods=["GET"])
 def get_menu():
     menu_data = [{"name":item.name, "price":item.price} for item in menu]
@@ -51,16 +68,24 @@ def get_menu():
 def create_order():
     data = request.get_json()
     username = data["username"]
-    user_address = data["address"]
     user_email = data["email"]
+    user_address = data["address"]
     selected_items = data["items"]
-
-    user = User(username, user_email, user_address)
+    if (user_address == "") and (any(
+        user.username == username and
+        user.email == user_email
+        for user in users
+    ) == False):
+        return jsonify({"message":f"User {username} isn't registered. Register or provide user address"}), 404       
     items = [item for item in menu if item.name in selected_items]
+    for user_to_find in users:
+        if user_to_find.username == username and user_to_find.email == user_email:
+            user_address = user_to_find["address"]
+    user = User(username, user_email, user_address)
     new_order = Order(user, items)
     orders.append(new_order)
-
     return jsonify({"message": "Order created successfully"})
+    
 
 @app.route("/order/<int:order_id>", methods=["GET"])
 def check_order_status(order_id):
@@ -76,6 +101,18 @@ def cancel_order(order_id):
             return jsonify({"message": "Order canceled!"})
         return jsonify({"message": "Cannot cancel ready order!"}), 400
     return jsonify({"message": "Order not found"}), 404
+
+@app.route("/user/register", methods=["POST"])
+def user_register():
+    data = request.get_json()
+    username = data["username"]
+    email = data["email"]
+    address = data["address"]
+    user = User(username, email, address)
+    if check_user_exists(user): #this should check if the user already exists and won't add a new one
+        return jsonify({"message":f"User {username} already exists"}), 400
+    users.append(user)
+    return jsonify({"message":f"User {username} registered"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
